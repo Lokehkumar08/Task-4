f0146f8105ede38bf58524b6932fae700aa060e9
K LOKESH KUMAR ACHARI <112178204+Lokehkumar08@users.noreply.github.com>
2025-02-11 12:59:44 +0530

    Add files via upload

---
+++ OrderModuleJSP-1/src/main/java/com/example/demo/OrderModuleJsp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderModuleJsp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderModuleJsp1Application.class, args);
+	}
+
+}
